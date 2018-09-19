4360ea80a7baa85c2263edbbdbbc27b76ae339c3
AdmiralOct <buildgamessnotbombs@gmail.com>
2018-09-18 23:23:22 -0400

    End of evening 9/18
    Cannon script is not semi working

---
+++ Assets/CannonWorkScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonWorkScript : MonoBehaviour {
+
+        public Vector3 pointB;
+        private Vector3 pointA;
+        void Start()
+        {
+
+            pointA = transform.position;
+           
+        }
+        void Update()
+    {
+        while (true)
+        {
+            MoveObject(transform, pointA, pointB, 3.0f);
+            MoveObject(transform, pointB, pointA, 3.0f);
+        }
+    }
+        void MoveObject(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
+        {
+            float i = 0.0f;
+            float rate = 1.0f / Time.time;
+            while (i < 1.0f) {
+                i += Time.deltaTime * rate;
+                thisTransform.position = Vector3.Lerp(startPos, endPos, i);
+                
+            }
+        }
+}
--- Assets/Scripts/CannonScript.cs
+++ Assets/Scripts/CannonScript.cs
@@ -10,18 +10,59 @@ public class CannonScript : MonoBehaviour {
 
     public bool isOn;
     public float fireRate = 2;
-    private float lastFireTime = float.MinValue;
 
-    private Vector2 target1;
-    private Vector2 target2;
+    public Vector3 target1;
+    public Vector3 target2;
     public bool send;
     public float speed;
-    void Start () {
+    public float rotateSpeed;
+
+    //Need this for rotation
+    private Transform target1Rot;
+    private Quaternion testRotation;
+    private Vector3 startPos;
+    private Vector3 distance;
+    private Vector3 currPos;
+    private float lastFireTime = float.MinValue;
+
+    //Testing out code
+    //private float startTime;
+    //private float journeyLength;
+    //private float distCovered;
+    //private float fracJourney;
+    //private float moveSpeed;
+    //[SerializeField] private GameObject pointA;
+    //[SerializeField] private GameObject pointB;
+    //[SerializeField] private Transform objectToUse;
+    //[SerializeField] private bool reverseMove = false;
+
+    void Start() {
         isOn = true;
-        target1 = new Vector2(3, 3);
-        target2 = new Vector2(-3, -3);
-        send = false;
-	}
+        //target1 = new Vector2(3, 3);
+        //target2 = new Vector2(-3, -3);
+        send = true;
+        startPos = gameObject.GetComponent<Transform>().position;
+
+        //Testing Code
+        //startTime = Time.time;
+        //journeyLength = Vector2.Distance(pointA.transform.position, pointB.transform.position);
+        //objectToUse = transform;
+
+        target1.x = 5;
+        target1.y = 5;
+        target1.z = 0;
+
+        //testRotation.SetEulerAngles(0, 0, 30);
+        testRotation = Quaternion.Euler(0, 0, 30);
+        //Vector3 rot = new Vector3(0, 0, 30);
+        //float angle = 0.0f;
+        //testRotation.ToAngleAxis(out angle, out rot);
+    }
+    //target1Rot = new Transform();
+    //target1Rot.eulerAngles = new Vector3(0, 0, 30);
+    
+
+        
 	
 	// Update is called once per frame
 	void Update () {
@@ -36,15 +77,60 @@ public class CannonScript : MonoBehaviour {
                 //bullet
                 lastFireTime = Time.time;
             }
+            //http://theflyingkeyboard.net/unity/unity-c-moving-gameobject-from-point-a-to-point-b/
+            /*distCovered = (Time.time - startTime) * moveSpeed;
+            fracJourney = distCovered / journeyLength;
 
+            if (reverseMove)
+            {
+                objectToUse.position = Vector2.Lerp(pointB.transform.position, pointA.transform.position, fracJourney);
+            }
+           else
+            {
+                objectToUse.position = Vector2.Lerp(pointA.transform.position, pointB.transform.position, fracJourney);
+            }
+            
+            if ((Vector2.Distance(objectToUse.position, pointB.transform.position) == 0.0f || Vector2.Distance(objectToUse.position, pointA.transform.position) == 0.0f)) //Checks if the object has travelled to one of the points
+            {
+                if (reverseMove)
+                {
+                    reverseMove = false;
+                }
+                else
+                {
+                    reverseMove = true;
+                }
+                startTime = Time.time;
+            }
+            */
             if (send)
-            {
-                transform.Translate(target2 * speed * Time.deltaTime);
-            }
-            else
-            {
-                transform.Translate(target1 * speed * Time.deltaTime);    
-            }
+             {
+                float angle = Mathf.Sin(Time.time) * 1; //tweak this to change frequency
+
+                //transform.localRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                //transform.rotation = Quaternion.FromToRotation(Vector3.forward, transform.forward);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, testRotation , rotateSpeed);
+                
+                distance = startPos - target1;
+                //transform.Translate(target1 * speed * Time.deltaTime);
+                    transform.position = Vector2.MoveTowards(transform.position, target1, speed);
+                 currPos = gameObject.GetComponent<Transform>().position;
+                 if (currPos == target1)
+                 {
+                     send = false;
+                 }
+             }
+             else
+             {
+                 distance = startPos - target1;
+                 //transform.Translate(target2 * speed * Time.deltaTime);
+                 transform.position = Vector2.MoveTowards(transform.position, startPos, speed);
+                 currPos = gameObject.GetComponent<Transform>().position;
+                 if (currPos == startPos)
+                 {
+                     send = true;
+                 }
+             }
         }
 	}
 
